edc895ddedb0110953908ef9d1c79dcda93eefa5
David Rose <david@drose.io>
2024-05-06 01:05:44 +0000

    improve non chat model

--- src/non_chat_app.py
+++ src/non_chat_app.py
@@ -6,14 +6,14 @@ app = FastAPI()
 pipe = pipeline("text-generation", "/mnt/store/llama-3-8b-bf16")
 
 @app.post("/generate")
-async def generate(input_data: str):
+async def generate(input_data: str, stop_token: str = None):
+    eos_token_id = pipe.tokenizer.convert_tokens_to_ids(stop_token) if stop_token else None
     outputs = pipe(
         input_data,
         max_new_tokens=128,
         temperature=0.01,
-        # eos_token_id=pipe.tokenizer.convert_tokens_to_ids("\\n"),
-        # eos_token_id=pipe.tokenizer.convert_tokens_to_ids("."),
+        eos_token_id=eos_token_id,
     )
     
-    out_str = outputs[0]["generated_text"].strip()
-    return {"generated_text": out_str, "tokens": outputs[0]["tokens"]}
+    n_tokens = len(pipe.tokenizer.encode(out_str))
+    return {"generated_text": out_str, "tokens": n_tokens}
