d5bb92e7de22589601b8c79a6afdbe27a2c64584
David Rose <david@drose.io>
2024-05-06 01:32:21 +0000

    chat app improvements

--- src/chat_app.py
+++ src/chat_app.py
@@ -11,23 +11,29 @@ class Message(BaseModel):
 
 class ChatInputData(BaseModel):
     messages: List[Message]
+    max_new_tokens: int = 2046
+    temperature: float = 0.01
 
 pipe = pipeline("text-generation", "/mnt/store/llama-3-8b-instruct-bf16")
 
 @app.post("/generate")
-async def generate(input_data: ChatInputData):
+async def generate(input_data: ChatInputData) -> dict:
+    if not hasattr(pipe, "tokenizer") or pipe.tokenizer is None:
+        raise ValueError("Tokenizer has not been loaded properly.")
+
+    messages_dict = [{"role": msg.role, "content": msg.content} for msg in input_data.messages]
     inputs = pipe.tokenizer.apply_chat_template(
-        input_data.messages, 
+        messages_dict, 
         add_generation_prompt=True, 
         tokenize=False
     )
     outputs = pipe(
-        inputs, 
-        max_new_tokens=128, 
+        inputs,
+        max_new_tokens=input_data.max_new_tokens, 
         eos_token_id=pipe.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-        temperature=0.01
+        temperature=input_data.temperature
     )
     
-    out_str = outputs[0]["generated_text"][len(inputs):].strip()
+    out_str = outputs[0]["generated_text"][len(inputs):].strip() # type: ignore
     n_tokens = len(pipe.tokenizer.encode(out_str))
     return {"generated_text": out_str, "tokens": n_tokens}
--- src/non_chat_app.py
+++ src/non_chat_app.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
